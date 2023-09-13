19b2963b704da4a8212a2c907e7a06d186274120
Sofia Elton <sofiaelton@MacBook-Pro-de-Sofia.local>
2023-09-13 19:16:21 +0800

    First commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import Suscribe from "./Suscribe";
+
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffers,
+  SuperQuality,
+  Suscribe,
+  Footer,
+  CustomerReviews,
+};
